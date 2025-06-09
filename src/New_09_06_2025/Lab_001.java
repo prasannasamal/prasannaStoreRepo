c3392b4d5bcb94571ef5e3f864946608fc379aaf
prasannasamal <prasannasamal47@gmail.com>
2025-06-09 11:01:48 +0530

    hello class ready to push now update

--- src/New_09_06_2025/Lab_001.java
+++ src/New_09_06_2025/Lab_001.java
@@ -2,6 +2,10 @@ package New_09_06_2025;
 
 public class Lab_001 {
     public static void main(String[] args) {
+        System.out.println(10+32);
+        System.out.println(10+42);
+        System.out.println(10+62);
+        System.out.println(10+1);
         System.out.println(10+12);
     }
 }
